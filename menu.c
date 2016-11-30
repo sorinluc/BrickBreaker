9a44d43bbf3e8831447768f39e9e79eb35476164
Lucas Sorin <sorin.luc@gmail.com>
2016-11-30 14:17:00 -0300

    Menu appears (in rose)

--- menu.c
+++ menu.c
@@ -2,13 +2,13 @@
 
 void menu_displayMenu (BITMAP* target) {
 	int menuitemcount = sizeof(menuItems) / sizeof(const char*);
+	int menuGap = 32;
 	int i=0;
 	for (i=0; i<menuitemcount; i++)
 	{
 		textprintf_ex(
-			target, font, menuItems[i],
-			SCREEN_W / 2, SCREEN_H / 2,
-			makecol(255, 0, 0), makecol(0, 0, 0));
+			target, font, SCREEN_W / 2, SCREEN_H / 4 + i * menuGap,
+			makecol(255, 100, 200), -1, menuItems[i]);
 	}
 
 }
