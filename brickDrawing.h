8533a062c19724fb5a184f0652b1aa0113d0ee40
Róger Luiz <rlom@cin.ufpe.br>
2016-12-01 14:55:23 -0300

    Update brickDrawing.h

--- brickDrawing.h
+++ brickDrawing.h
@@ -11,7 +11,21 @@ struct brickers {
   int bit;
 };
 
-struct brickers brick[8][10];
+struct bol {
+  int bolX;
+	int bolY;
+	int bolR;
+  int psB;
+  int psL;
+};
 
+struct brickers brick[8][10];
+struct bol bola;
+int LARG = 1920;
+int ALT = 1080;
+int RAIO = 20;
+
+void printBrick();
 void povoamento(BITMAP* buffer);
 void breakbrick(BITMAP* buffer, int Bi, int Bj);
+void bolinha();
