9cf9bb05ba7cf3c3afa8de1a30b64d0fd15b1def
Róger Luiz <rlom@cin.ufpe.br>
2016-12-01 13:59:21 -0300

    main 2.0
    Algumas correções no loop

--- main.c
+++ main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <allegro.h>
-#include "menu.h"
-
-volatile int exit_program;
+#include "brickDrawing.h"
 
 
 int main()
 {
-   allegro_init(); //Inicializa itens básicos da biblioteca
-   install_timer(); //Timers necessários para áudio, mouse...
-   install_keyboard(); //Instalando teclado
-   set_color_depth(32); //Setar profundidade das cores (32 bits p/ combinações de cores
-   set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1920, 1080, 0, 0); //Setar Vídeo (Driver de vídeo, 800x600 de resolução, 0 e 0 parâmetros para tela virtual) 
-   set_window_title("Brick Braker"); //Título da janela
-
-  while(!exit_program)
+  
+  allegro_init(); //Inicializa itens básicos da biblioteca
+  install_timer(); //Timers necessários para áudio, mouse...
+  install_keyboard(); //Instalando teclado
+  set_color_depth(32); //Setar profundidade das cores (32 bits p/ combinações de cores
+  set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1920, 1080, 0, 0); //Setar Vídeo (Driver de vídeo, 800x600 de resolução, 0 e 0 parâmetros para tela virtual) 
+  set_window_title("Brick Breaker"); //Título da janela
+  
+  BITMAP* buffer = create_bitmap(SCREEN_W, SCREEN_H);
+  int exit_program = 0;
+  povoamento(buffer);
+  
+  int x = 0, y = 0;
+  
+  while(exit_program == 0)
   {
-     //INPUT
-   if(key[KEY_ESC])
-      exit_program = TRUE;
-   menu_displayMenu(screen);
-	menu_updateMenu();
-
+  //INPUT
+    if(key[KEY_ESC]){
+      exit_program = 1;
+    }
+  //UPDATE
+    if(key[KEY_M]){
+      breakbrick(buffer, x, y);
+      x++;
+      y++;
+    }
+  //DRAW
   }
+
+  //FINALIZAÇÃO
+  destroy_bitmap(buffer);
   return 0;
 }
 END_OF_MAIN();
-
-//gcc -Wall <arquivo.c> -o programa -lalleg
