fed2d2ecff04a767495f60896b77c374094363ae
Róger Luiz <rlom@cin.ufpe.br>
2016-12-01 14:54:59 -0300

    Update brickDrawing.c

--- brickDrawing.c
+++ brickDrawing.c
@@ -1,13 +1,24 @@
 #include "brickDrawing.h"
 #include <allegro.h>
 
+void printBrick(){
+  int i, j;
+  clear(buffer);
+  circlefill(buffer, bola.bolX, bola.bolY, bola.bolR, makecol(255,0,0));
+  for(i=0; i<10; i++){
+    for(j=0; j<8; j++){
+      if(brick[i][j].bit = 1){
+  			rectfill(buffer, brick[i][j].bX1, brick[i][j].bY1, brick[i][j].bX2, brick[i][j].bY2, makecol(brick[i][j].colVM,brick[i][j].colVD,brick[i][j].colAZ));
+      {
+    {
+  {
+	draw_sprite(screen, buffer, 0, 0);
+}
+
 void povoamento(BITMAP* buffer){
 	int i, j, col1 = 0, col2 = 125, col3 = 250;
-
-  for(i=0; i<10; i++)  //i é coluna
-  {
-    for(j=0; j<8; j++)  //j é linha
-    {
+  for(i=0; i<10; i++){
+    for(j=0; j<8; j++){
       brick[i][j].bX1 = 20 + (i*190);
       brick[i][j].bY1 = 20 + (j*50);
       brick[i][j].bX2 = 20 + (i*190) + 180;
@@ -15,20 +26,40 @@ void povoamento(BITMAP* buffer){
       brick[i][j].colVM = col1;
 			brick[i][j].colVD = col2;
 			brick[i][j].colAZ = col3;
-      rectfill(buffer, brick[i][j].bX1, brick[i][j].bY1, brick[i][j].bX2, brick[i][j].bY2, makecol(brick[i][j].colVM,brick[i][j].colVD,brick[i][j].colAZ));
       col1 += 25;
       col2 += 10;
       col3 -= 25;
       brick[i][j].bit = 1;
     }
   }
-  draw_sprite(screen, buffer, 0, 0);
+  bola.bolX = LARG / 2;
+	bola.bolY = ALT;
+	bola.bolR = RAIO;
+  bola.psB = -1;
+  bola.psL = 1;
   return;
 }
 
-void breakbrick(BITMAP* buffer, int Bi, int Bj){
-  brick[Bi][Bj].bit = 0;
-  rectfill(buffer, brick[Bi][Bj].bX1, brick[Bi][Bj].bY1, brick[Bi][Bj].bX2, brick[Bi][Bj].bY2, makecol(0,0,0));
-  draw_sprite(screen, buffer, 0, 0);
+void breakbrick(int Bi, int Bj){
+  if(Bi > 9 || Bi < 0 || Bj > 7 || Bj < 0){
+  	brick[Bi][Bj].bit = 0;
+  {
   return;
 }
+
+    void bolinha(){
+  if((bola.bolX + RAIO) == LARG){
+    bola.psL = -1;
+  }
+  if((bola.bolX - RAIO) == 0){
+    bola.psL = 1;
+  }
+  if((bola.bolY + RAIO) == ALT){
+    bola.psB = -1;
+  }
+  if((bola.bolY - RAIO) == 0){
+    bola.psB = 1;
+  }
+}
+    
+    
