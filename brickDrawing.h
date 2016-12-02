bd21d1e3463dfaedad213a593561e82fda5b6b5e
ajmbf <ajmbf@cin.ufpe.br>
2016-12-01 21:13:07 -0300

    Functional !

--- brickDrawing.h
+++ brickDrawing.h
@@ -17,15 +17,43 @@ struct bol {
 	int bolR;
   int psB;
   int psL;
+  int incA;
+  int incB;
 };
 
-struct brickers brick[8][10];
-struct bol bola;
-int LARG = 1920;
-int ALT = 1080;
-int RAIO = 20;
+struct bar {
+  int barX1;
+	int barY1;
+	int barX2;
+  int barY2;
+};
 
-void printBrick();
-void povoamento(BITMAP* buffer);
-void breakbrick(BITMAP* buffer, int Bi, int Bj);
+struct colisao {
+  int setC, setB, setE, setD;
+};
+
+#define Plinha 3
+#define Ulinha 7
+#define Nlinha 10
+#define Pcol 3
+#define Ucol 20
+#define Ncol 22
+#define VEL 1
+#define RAIO 20
+#define LARG 1920
+#define ALT 900
+#define MeB 10
+#define Blarg (((LARG - 40) / (Ncol)) - MeB)
+#define Balt ((((ALT/2) - 20) / (Nlinha)) - MeB)
+
+struct brickers brick[Ucol][Ulinha];
+struct bol bola;
+struct bar barra;
+struct colisao col [Ucol][Ulinha];
+
+void printBrick(BITMAP* buffer);
+void povoamento();
+void breakbrick();
 void bolinha();
+void barrinha();
+void barrabrick();
